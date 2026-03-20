2c8837fa64b816aefdda319baf492b7ea0529656
Kanyon05 <windtree01@gmail.com>
2026-03-20 11:16:43 +0900

    Add divide function to calculate quotient and remainder

--- code/2-3.c
+++ code/2-3.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+void divide(int a, int b, int *q, int *r_ ) {
+    *q = a / b;
+    *r_ = a % b;
+}
+
 int main() {
+    int a = 27, b = 4, q=0, r=0;
+
+    divide(a, b, &q, &r);
+
+    printf("%d\n%d\n", q, r);
 
     return 0;
 }
